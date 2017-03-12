7f05211d282502078b37998c71427ab1e1c45fe8
Sam Kozloff <samkozloff89@gmail.com>
2017-03-12 16:01:58 -0500

    basic entry functinoality working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from entries import entries
 
 app = Flask(__name__)
@@ -11,4 +11,23 @@ def show_entries():
 
 @app.route('/add', methods=['POST'])
 def add_entry():
+    entries.append({"description" : request.form['description'], "template" : request.form['template']})
+    print(entries)
     return redirect(url_for('show_entries'))
+
+
+
+# @app.route('/add', methods=['POST'])
+# def add_entry():
+#     # session was imported above
+#     if not session.get('logged_in'):
+#         abort(401)
+#     db = get_db()
+#     # what does request.form do?
+#     db.execute('insert into entries (title, text) values (?, ?)',
+#                  [request.form['title'], request.form['text']])
+#     db.commit()
+#     # what does this do?
+#     flash('New entry was successfully posted')
+#     # so you can redirect to the show_entries function, interesting
+#     return redirect(url_for('show_entries'))
