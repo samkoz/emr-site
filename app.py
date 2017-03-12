e10551c782f53d654edbfc78ba620d49c67efcba
Sam Kozloff <samkozloff89@gmail.com>
2017-03-12 15:45:21 -0500

    got table formatted, working on adding entry functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from entries import entries
 
 app = Flask(__name__)
@@ -6,7 +6,9 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 @app.route('/')
-def hello_world():
-    for entry in entries:
-        print(entry)
+def show_entries():
     return render_template('entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    return redirect(url_for('show_entries'))
