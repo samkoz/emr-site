f6d141141c8adc9d4417d3d395a128cc351505cd
Sam Kozloff <samkozloff89@gmail.com>
2017-03-24 12:42:30 -0500

    added flask-bootstrap; delete / save not working anymore

--- app/app.py
+++ app/app.py
@@ -1,3 +1,7 @@
 from .factory import create_app
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = create_app(__name__)
+moment = Moment(app)
+bootstrap = Bootstrap(app)
